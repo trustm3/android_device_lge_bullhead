62b1434131e534294e1dadcef7d0ee41501667ad
Michael Weiß <michael.weiss@aisec.fraunhofer.de>
2018-03-09 14:28:01 +0100

    Android.mk: also build for trustme targets

--- Android.mk
+++ Android.mk
@@ -23,7 +23,8 @@
 # *not* include it on all devices, so it is safe even with hardware-specific
 # components.
 
-ifneq ($(filter bullhead, $(TARGET_DEVICE)),)
+#ifneq ($(filter bullhead, $(TARGET_DEVICE)),)
+ifneq ($(filter bullhead trustme_bullhead_%, $(TARGET_DEVICE)),)
 
 LOCAL_PATH := $(call my-dir)
 
